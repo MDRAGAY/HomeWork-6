ff53f8f7d70fedbaae7f1cd8909f543ee774532b
mdrag <m.dragay@yandex.ru>
2023-01-14 11:18:45 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main (String[] args) {
         task1();
         task2();
+        task3();
+
     }
     public static void task1 (){System.out.println("Задача 1");
         for (int i = 0; i < 10; i = i + 1) {
@@ -11,6 +13,11 @@ public class Main {
     public static void task2(){System.out.println("Задача 2");
         for (int i = 10; i > 0; i = i - 1) {
             System.out.println("Итерация цикла "  +  i);}
+    }
+    public static void task3() {System.out.println("Задача 3");
+        for (int a = 0; a <= 17; a = a + 2) {
+            System.out.println("Итерация цикла "  +  a);
+        }
     }
 
 }
